49becc2a8fae3f9f67776df379ced9618334a850
SmailSaidani <94194781+SmailSaidani@users.noreply.github.com>
2024-09-05 15:34:05 +0100

    Update FastClicker.py

--- FastClicker.py
+++ FastClicker.py
@@ -10,7 +10,7 @@ back = (200, 255, 255) #background color
 mw = pygame.display.set_mode((500, 500)) #main window
 mw.fill(back)
 clock = pygame.time.Clock()
-
+print("hello")
 
 '''rectangle class'''
 
